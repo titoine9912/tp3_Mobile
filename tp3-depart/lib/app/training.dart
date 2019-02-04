d950aa8dcb013c28488671f2bb986abe3e6d32ef
1463457 <1463457@cegep-ste-foy.qc.ca>
2019-02-04 09:02:09 -0500

    c

---
+++ tp3-depart/lib/app/training.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:tp3/app/hiragana/hiragana.dart';
+
+class Training extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _TrainingState();
+  }
+}
+
+class _TrainingState extends State<Training> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: GridView.count(
+        crossAxisCount: null,
+        children: List<Widget>.generate(Hiraganas.length, (index) {
+          return GridTile(
+            child: FittedBox(
+              child: Hiraganas,
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
