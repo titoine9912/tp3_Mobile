7ce6e0ce9debfbb0256d51be929612bbef473b49
1463457 <1463457@cegep-ste-foy.qc.ca>
2019-02-04 08:22:01 -0500

    Update main.dart

--- tp3-depart/lib/main.dart
+++ tp3-depart/lib/main.dart
@@ -2,13 +2,3 @@ import 'package:flutter/material.dart';
 import 'package:tp3/app/app.dart';
 
 void main() => runApp(App());
-
-class App extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TP3',
-      home: Home(),
-    );
-  }
-}
