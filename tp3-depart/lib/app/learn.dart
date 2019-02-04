5144e9ea999d1bb3513fcfd365f6272d6b8bd36d
1463457 <1463457@cegep-ste-foy.qc.ca>
2019-02-04 08:38:54 -0500

    Update learn.dart

---
+++ tp3-depart/lib/app/learn.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Learn extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return null;
+  }
+
+}
+
+class _LearnState extends State<Lean> {
+
+}
